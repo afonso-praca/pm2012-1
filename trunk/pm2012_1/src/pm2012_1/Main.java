fb4fc6914a83b513cbfee9120a0dd349d7dcaf35
paulo.inacio@uniriotec.br <paulo.inacio@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-05-08 00:38:18 +0000

    Criação do Repositório

---
+++ trunk/pm2012_1/src/pm2012_1/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pm2012_1;
+
+/**
+ *
+ * @author PC
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+        // TODO code application logic here
+    }
+
+}
