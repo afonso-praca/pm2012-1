6848dbf20ffc3f444c8b8c2bef708c5f82d3aecf
tais.cordeiro@uniriotec.br <tais.cordeiro@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-05-12 17:28:50 +0000

    Alterando o nome da classe para retirar caracter especial.

---
+++ pm2012_1/src/pm2012_1/Preco.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pm2012_1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Taís
+ */
+public class Preco {
+    
+        private Date Data;
+	private double PrecoPA;
+	private double PrecoPB;
+	private double PrecoPC;
+	
+	public Date getData() {
+		return Data;
+	}
+	public void setData(Date data) {
+		Data = data;
+	}
+	public double getPrecoPA() {
+		return PrecoPA;
+	}
+	public void setPrecoPA(double precoPA) {
+		PrecoPA = precoPA;
+	}
+	public double getPrecoPB() {
+		return PrecoPB;
+	}
+	public void setPrecoPB(double precoPB) {
+		PrecoPB = precoPB;
+	}
+	public double getPrecoPC() {
+		return PrecoPC;
+	}
+	public void setPrecoPC(double precoPC) {
+		PrecoPC = precoPC;
+	}
+
+}
