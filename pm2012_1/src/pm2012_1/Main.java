71f656ac88a678e7557ccfe2e21fd65f9faa6b2b
paulo.inacio@uniriotec.br <paulo.inacio@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-05-12 19:01:07 +0000

    Primeiro commit

--- pm2012_1/src/pm2012_1/Main.java
+++ pm2012_1/src/pm2012_1/Main.java
@@ -5,6 +5,9 @@
 
 package pm2012_1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  *
  * @author PC
@@ -14,10 +17,80 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+      public static void main(String[] args) {
 
-        System.out.println("Hello World 2");
-        // TODO code application logic here
+        //load files
+        loadVendas();
+        loadPrecos();
+        loadVendedores();
+
+        //calc and generate output file
+        calcCommission();
+        buildCommissionFile();
+
+        System.out.println("Application Started!");
+    }
+
+    private static void loadVendas() {
+
+        BufferedReader vendasReader = null;
+        try {
+            try {
+                vendasReader = new BufferedReader(
+                        new FileReader("src/pm2012_1/vendas.txt"));
+                System.out.println(vendasReader.readLine());
+
+            } finally {
+                if (vendasReader != null)
+                    vendasReader.close();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    private static void loadVendedores() {
+
+        BufferedReader vendedoresReader = null;
+        try {
+            try {
+                vendedoresReader = new BufferedReader(
+                        new FileReader("src/pm2012_1/vendedores.txt"));
+                System.out.println(vendedoresReader.readLine());
+
+            } finally {
+                if (vendedoresReader != null)
+                    vendedoresReader.close();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    private static void loadPrecos() {
+
+        BufferedReader precosReader = null;
+        try {
+            try {
+                precosReader = new BufferedReader(
+                        new FileReader("src/pm2012_1/precos.txt"));
+                System.out.println(precosReader.readLine());
+
+            } finally {
+                if (precosReader != null)
+                    precosReader.close();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    private static void calcCommission(){
+
+    }
+
+    private static void buildCommissionFile(){
+
     }
 
 }
