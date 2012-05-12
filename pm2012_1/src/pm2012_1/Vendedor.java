a19ab317556d00deb465150748d54db8e7b26fdc
tais.cordeiro@uniriotec.br <tais.cordeiro@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-05-12 15:00:48 +0000

    Criação da classe Vendedor.

---
+++ pm2012_1/src/pm2012_1/Vendedor.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pm2012_1;
+
+/**
+ *
+ * @author Taís
+ */
+public class Vendedor {
+    
+    	private String Codigo;
+	private String Nome;
+	private char Categoria;
+	
+	public String getCodigo() {
+		return Codigo;
+	}
+        
+	public void setCodigo(String codigo) {
+		Codigo = codigo;
+	}
+        
+	public String getNome() {
+		return Nome;
+	}
+        
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+        
+	public char getCategoria() {
+		return Categoria;
+	}
+        
+	public void setCategoria(char categoria) {
+		Categoria = categoria;
+	}
+	
+
+}
