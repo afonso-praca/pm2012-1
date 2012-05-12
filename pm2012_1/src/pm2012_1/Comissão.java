04a63dcc6d282216e32155926a96cb8e0c99b9d2
tais.cordeiro@uniriotec.br <tais.cordeiro@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-05-12 15:18:11 +0000

    Adição da classe Comissão

---
+++ pm2012_1/src/pm2012_1/Comissão.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pm2012_1;
+
+/**
+ *
+ * @author Taís
+ */
+public class Comissão {
+    
+        private String Mes;
+	private Vendedor Vendedor;
+	private double TotalVendidoProdutoA;
+	private double TotalVendidoProdutoB;
+	private double TotalVendidoProdutoC;
+	private double Total;
+	private double Comissao;
+	
+	public String getMes() {
+		return Mes;
+	}
+        
+	public void setMes(String mes) {
+		Mes = mes;
+	}
+        
+	public Vendedor getVendedor() {
+		return Vendedor;
+	}
+        
+	public void setVendedor(Vendedor vendedor) {
+		Vendedor = vendedor;
+	}
+        
+	public double getTotalVendidoProdutoA() {
+		return TotalVendidoProdutoA;
+	}
+        
+	public void setTotalVendidoProdutoA(double totalVendidoProdutoA) {
+		TotalVendidoProdutoA = totalVendidoProdutoA;
+	}
+        
+	public void setSumTotalVendidoProdutoA(double totalVendidoProdutoA) {
+		TotalVendidoProdutoA += totalVendidoProdutoA;
+	}
+        
+	public double getTotalVendidoProdutoB() {
+		return TotalVendidoProdutoB;
+	}
+        
+	public void setTotalVendidoProdutoB(double totalVendidoProdutoB) {
+		TotalVendidoProdutoB = totalVendidoProdutoB;
+	}
+        
+	public double getTotalVendidoProdutoC() {
+		return TotalVendidoProdutoC;
+	}
+        
+	public void setTotalVendidoProdutoC(double totalVendidoProdutoC) {
+		TotalVendidoProdutoC = totalVendidoProdutoC;
+	}
+        
+	public double getTotal() {
+		return TotalVendidoProdutoA + TotalVendidoProdutoB + TotalVendidoProdutoC;
+	}
+        
+	public double getComissao() {
+		return Comissao;
+	}
+        
+	public void setComissao(double comissao) {
+		Comissao = comissao;
+	}
+        
+	public void setSumTotalVendidoProdutoB(double totalVendidoProdutoB2) {
+		TotalVendidoProdutoB+= totalVendidoProdutoB2;
+		
+	}
+        
+	public void setSumTotalVendidoProdutoC(double totalVendidoProdutoC2) {		
+		TotalVendidoProdutoC+=totalVendidoProdutoC2;		
+	}
+
+}
