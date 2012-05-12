0ecd6dcafb30c19c01cb44ebdacfe266dfaa6d9e
marina.vinhaes@uniriotec.br <marina.vinhaes@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-05-12 20:32:08 +0000

    Alteaçao Categorias

--- trunk/pm2012_1/src/pm2012_1/Vendedor.java
+++ trunk/pm2012_1/src/pm2012_1/Vendedor.java
@@ -9,11 +9,12 @@ package pm2012_1;
  * @author Taís
  */
 public class Vendedor {
+        public enum Categorias { CATEGORIA1 , CATEGORIA2 ; }
     
     	private String Codigo;
 	private String Nome;
-	private char Categoria;
-	
+	private Categorias categoria;
+
 	public String getCodigo() {
 		return Codigo;
 	}
@@ -29,14 +30,12 @@ public class Vendedor {
 	public void setNome(String nome) {
 		Nome = nome;
 	}
-        
-	public char getCategoria() {
-		return Categoria;
-	}
-        
-	public void setCategoria(char categoria) {
-		Categoria = categoria;
-	}
-	
 
+        public Categorias getCategoria() {
+            return categoria;
+        }
+
+        public void setCategoria(Categorias categoria) {
+            this.categoria = categoria;
+        }
 }
