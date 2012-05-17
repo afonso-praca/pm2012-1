d9f7143bcc200d125cf1cc045645763e92547ad9
paulo.inacio@uniriotec.br <paulo.inacio@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-05-17 04:23:32 +0000

    Alteração de leitura do arquivo de vendedores.

--- trunk/pm2012_1/src/pm2012_1/Vendedor.java
+++ trunk/pm2012_1/src/pm2012_1/Vendedor.java
@@ -9,35 +9,54 @@ package pm2012_1;
  * @author Taís
  */
 public class Vendedor {
-        public enum Categorias { CATEGORIA1 , CATEGORIA2 ; }
+
     
-    	private String Codigo;
-	private String Nome;
-	private Categorias categoria;
+        public enum Categoria { CATEGORIA1 , CATEGORIA2 ; }
+    
+    	private String codigo;
+	private String nome;
+	private int categoria;
 
-	public String getCodigo() {
-		return Codigo;
-	}
-        
-	public void setCodigo(String codigo) {
-		Codigo = codigo;
-	}
-        
-	public String getNome() {
-		return Nome;
-	}
-        
-	public void setNome(String nome) {
-		Nome = nome;
-	}
+    public Vendedor(String codigo, String nome, int categoria) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.categoria = categoria;
+    }
 
-        public Categorias getCategoria() {
-            return categoria;
-        }
+    public int getCategoria() {
+        return categoria;
+    }
 
-        public void setCategoria(Categorias categoria) {
-            this.categoria = categoria;
-        }
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
 
         
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+
+
+            @Override
+    public String toString() {
+        return "Vendedor{" + "Código=" + codigo + ", Nome=" + nome + ", Categoria=" + categoria + "" ;
+    }
+
 }
