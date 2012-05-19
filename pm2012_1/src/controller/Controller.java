af95ecdefec2401efb9c5f8dbc6f89d3978dba46
afonsoinfo@gmail.com <afonsoinfo@gmail.com@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-05-19 19:48:05 +0000

    refatorando nomes de classes e colocando a classe de claculo na pasta correta

--- pm2012_1/src/controller/Controller.java
+++ pm2012_1/src/controller/Controller.java
@@ -4,10 +4,9 @@
  */
 package controller;
 
-import model.CalculoComissao;
-import model.Preco;
-import model.Venda;
-import model.Vendedor;
+import model.Price;
+import model.Sale;
+import model.Seller;
 
 /**
  *
@@ -16,16 +15,16 @@ import model.Vendedor;
 public class Controller {
     
     //arrays com os itens carregados
-    private static Preco[] precos;
-    private static Venda[] vendas;
-    private static Vendedor[] vendedores;
+    private static Price[] precos;
+    private static Sale[] vendas;
+    private static Seller[] vendedores;
         
     //functions
     public static void init(String mes, String precos, String vendas, String vendedores, String saida){
         //load files
-        loadFiles.loadVendas(vendas);
-        loadFiles.loadPrecos(precos);
-        loadFiles.loadVendedores(vendedores);
+        LoadFiles.loadVendas(vendas);
+        LoadFiles.loadPrecos(precos);
+        LoadFiles.loadVendedores(vendedores);
     }
     
     public static void calc(String mes, String precos, String vendas, String vendedores, String saida){
@@ -33,7 +32,7 @@ public class Controller {
         int dataentrada = Integer.parseInt(mes);
         int produto = Integer.parseInt(vendas);
 
-        CalculoComissao calcComissao = new CalculoComissao();
+        CalcCommission calcComissao = new CalcCommission();
         
         for (data = 0; data <= 12; data++){
             if (data == dataentrada){
