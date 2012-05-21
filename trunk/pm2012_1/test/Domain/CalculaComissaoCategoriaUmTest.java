f626d3a39e3d05ef095d51f5fa156616dcd9abc7
tais.cordeiro@uniriotec.br <tais.cordeiro@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-05-21 06:35:03 +0000

    Classe de teste CalculaComissaoCategoriaUmTest

---
+++ trunk/pm2012_1/test/Domain/CalculaComissaoCategoriaUmTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import controller.CalculaComissaoCategoriaUm;
+import model.Comissao;
+import controller.CalculaComissaoCategoriaUm;
+import controller.CalculaComissaoCategoriaUm;
+import model.Comissao;
+/**
+ *
+ * @author Taís
+ */
+
+public class CalculaComissaoCategoriaUmTest extends CalculaComissaoTesteBase{
+
+	Comissao comissao;
+	
+	@Before
+	public void setUp() throws Exception{
+		super.setUp();
+		comissao = new Comissao();
+		comissao.setMes("03");
+		comissao.setVendedor(Vendedores.get(0));
+		
+	}
+	
+	@Test
+	public void DeveSerPossivelCalcularUmaComissao() {
+		
+		comissao.setTotalVendidoProdutoA(400);
+		comissao.setTotalVendidoProdutoB(1000);
+		comissao.setTotalVendidoProdutoC(600);
+		
+		CalculaComissaoCategoriaUm calculador = new CalculaComissaoCategoriaUm();
+		
+		calculador.CalculaComissao(comissao);
+		
+		assertEquals(260, comissao.getComissao(),0.0);
+		
+	}
+
+}
