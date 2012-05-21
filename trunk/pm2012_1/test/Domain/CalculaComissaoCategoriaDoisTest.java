c2fbd2c1948618a3612a53816c6487f8994d866e
tais.cordeiro@uniriotec.br <tais.cordeiro@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-05-21 06:33:50 +0000

    Classe de teste CalculaComissaoCategoriaDoisTest

---
+++ trunk/pm2012_1/test/Domain/CalculaComissaoCategoriaDoisTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import controller.CalculaComissaoCategoriaDois;
+import controller.CalculaComissaoCategoriaUm;
+import model.Comissao;
+
+/**
+ *
+ * @author Taís
+ */
+import controller.CalculaComissaoCategoriaDois;
+import controller.CalculaComissaoCategoriaDois;
+import controller.CalculaComissaoCategoriaUm;
+import model.Comissao;
+
+public class CalculaComissaoCategoriaDoisTest extends CalculaComissaoTesteBase{
+
+
+	Comissao comissao;
+	
+	@Before
+        @Override
+	public void setUp() throws Exception{
+		super.setUp();
+		comissao = new Comissao();
+		comissao.setMes("03");
+		comissao.setVendedor(Vendedores.get(1));
+		
+	}
+	
+	@Test
+	public void DeveSerPossivelCalcularUmaComissao() {
+		
+		comissao.setTotalVendidoProdutoA(1500);
+		comissao.setTotalVendidoProdutoB(2000);
+		comissao.setTotalVendidoProdutoC(1500);
+		
+		CalculaComissaoCategoriaDois calculador = new CalculaComissaoCategoriaDois();
+		
+		calculador.CalculaComissao(comissao);
+		
+		assertEquals(1500, comissao.getComissao(),0.0);
+		
+	}
+
+}
