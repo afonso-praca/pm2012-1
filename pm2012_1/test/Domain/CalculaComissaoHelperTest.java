8877a326298bb97536175876dbb4f609e3372334
tais.cordeiro@uniriotec.br <tais.cordeiro@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-05-21 06:35:43 +0000

    Classe de teste CalculaComissaoHelperTest

---
+++ pm2012_1/test/Domain/CalculaComissaoHelperTest.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+import model.Preco;
+import model.Venda;
+import model.Vendedor;
+import java.util.HashMap;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import controller.CalculaComissaoHelper;
+import controller.CalculaComissaoHelper;
+import model.Comissao;
+
+public class CalculaComissaoHelperTest extends CalculaComissaoTesteBase{
+
+	private CalculaComissaoHelper calculadorHelper;
+	
+	@Before
+	public void Init() throws Exception{
+		super.setUp();
+		calculadorHelper = new CalculaComissaoHelper();
+	}
+	
+	@Test
+	public void SelecionaVendasDoMesTeste() {
+		
+		//ARRANGE		
+		String mesParaSelecionar = "03";
+		
+		AdicionaUmaVendaAListaDeVendas("10/04/2011", "0020", 5, 10, 15); //Venda de outro mes sem ser o mes da busca
+		
+		//ACT	
+		
+		List<Venda> vendasActual = calculadorHelper.SelecionaVendasDoMes(Vendas, mesParaSelecionar);
+		
+		//ASSERT
+		
+		assertNotNull(vendasActual);
+		assertEquals(3, vendasActual.size());		
+		
+	}
+	
+	@Test
+	public void SelecionaValoresParaAComissaoTeste(){
+		
+		String mesParaSelecionar = "03";
+		
+		List<Comissao> comissoesACalcular = calculadorHelper.SelecionaValoresParaAComissao(Vendas, Precos, Vendedores,mesParaSelecionar);
+		
+		assertNotNull(comissoesACalcular);
+		assertEquals(2, comissoesACalcular.size());
+		
+	}
+
+	@Test
+	public void DeveSerPossivelSelecionarValoresParaAComissaoComTotaisCorretos(){
+		
+		String mesParaSelecionar = "03";
+				
+		double valorTotalProdutoAVendedor0020Expected= 50*40.0;
+		double valorTotalProdutoBVendedor0019Expected = 35*50;
+		
+		List<Comissao> comissoesACalcular = calculadorHelper.SelecionaValoresParaAComissao(Vendas, Precos, Vendedores,mesParaSelecionar);
+		
+		assertNotNull(comissoesACalcular);
+		
+		double totalVendedorAProdutoA = 0;
+		double totalVendedorBProdutoB= 0;
+		
+		for (Comissao comissao : comissoesACalcular){
+			
+			if(comissao.getVendedor().getCodigo().equals("0020")){
+				
+				totalVendedorAProdutoA= comissao.getTotalVendidoProdutoA();
+			}
+			else if(comissao.getVendedor().getCodigo().equals("0019")){
+				
+				totalVendedorBProdutoB = comissao.getTotalVendidoProdutoB();				
+			}			
+		}
+		
+		assertEquals(valorTotalProdutoAVendedor0020Expected, totalVendedorAProdutoA, 0.0);
+		assertEquals(valorTotalProdutoBVendedor0019Expected, totalVendedorBProdutoB, 0.0);
+	}
+
+	
+}
