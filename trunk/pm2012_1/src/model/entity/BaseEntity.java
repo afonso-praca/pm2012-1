718a3c40e366331f5255b9d2f3f2ab116e6584db
afonsoinfo@gmail.com <afonsoinfo@gmail.com@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-06-19 23:11:39 +0000

    classes de entity com serializable

---
+++ trunk/pm2012_1/src/model/entity/BaseEntity.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.entity;
+
+import java.io.Serializable;
+
+/**
+ * Superclasse de todas as entidades da aplicação.
+ * Pode conter código comum a todas as entidades.
+ * 
+ * @author Afonso
+ *
+ */
+
+public class BaseEntity implements Serializable {
+    private static final long serialVersionUID = 4814191972881312483L;
+}
---
+++ trunk/pm2012_1/src/model/entity/Vendedor.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.entity;
+
+/**
+ *
+ * @author Taís
+ */
+public class Vendedor extends BaseEntity {
+
+
+	private String Codigo;
+	private String Nome;
+	private char Categoria;
+	
+	public String getCodigo() {
+		return Codigo;
+	}
+	public void setCodigo(String codigo) {
+		Codigo = codigo;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public char getCategoria() {
+		return Categoria;
+	}
+	public void setCategoria(char categoria) {
+		Categoria = categoria;
+	}
+	
+	
+}
+
+
