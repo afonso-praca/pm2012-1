f92b5bd653cec07bf364177b8d39b5c5ea78076b
afonsoinfo@gmail.com <afonsoinfo@gmail.com@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-06-20 00:23:22 +0000

    Classe genérica para leitor de entidades

---
+++ trunk/pm2012_1/src/model/VendaToEntity.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import model.entity.Venda;
+
+/**
+ *
+ * @author labccet
+ */
+public class VendaToEntity extends CSVToEntity<Venda> {
+    
+    public VendaToEntity(){
+        sdf = new SimpleDateFormat("dd/MM/yyyy");
+    }
+    
+    public List<Venda> ConverteArquivoDoModelo(String caminho){
+        ConverteArquivo(caminho);
+        List<Venda> listaVendasDoArquivo = new ArrayList<Venda>();
+
+        for(String[] conteudoLinhaDoArquivo : ArquivoEmLista){
+            Venda venda = new Venda();
+            try {
+                venda.setData(sdf.parse(conteudoLinhaDoArquivo[0]));
+                venda.setCodigoVendedor(conteudoLinhaDoArquivo[1]);
+                venda.setQuantidadePA(Integer.parseInt(conteudoLinhaDoArquivo[2]));
+                venda.setQuantidadePB(Integer.parseInt(conteudoLinhaDoArquivo[3]));
+                venda.setQuantidadePC(Integer.parseInt(conteudoLinhaDoArquivo[4]));
+            } catch (ParseException e) {
+                System.out.println(e.getMessage());
+                return null;
+            }
+            
+            listaVendasDoArquivo.add(venda);
+        }
+        
+        return listaVendasDoArquivo;
+    }
+    
+}
