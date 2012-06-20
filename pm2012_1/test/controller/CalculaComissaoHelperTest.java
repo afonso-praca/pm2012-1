2bba9f5342feb4eadf552e21e45b62e4b526576d
afonsoinfo@gmail.com <afonsoinfo@gmail.com@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-06-20 14:41:50 +0000

    ajustes nas classes de teste. adicionado import static do assert. e adicionadas funcoes de criacao de objs na classe base que cria os cenarios de teste

--- pm2012_1/test/controller/CalculaComissaoHelperTest.java
+++ pm2012_1/test/controller/CalculaComissaoHelperTest.java
@@ -8,79 +8,75 @@ import java.util.List;
 import model.CalculaComissaoHelper;
 import model.entity.Comissao;
 import model.entity.Venda;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class CalculaComissaoHelperTest extends CalculaComissaoTesteBase{
 
-	private CalculaComissaoHelper calculadorHelper;
-	
-	@Before
-	public void Init() throws Exception{
-		super.setUp();
-		calculadorHelper = new CalculaComissaoHelper();
-	}
-	
-	@Test
-	public void SelecionaVendasDoMesTeste() {
-		
-		//ARRANGE		
-		String mesParaSelecionar = "03";
-		
-		AdicionaUmaVendaAListaDeVendas("10/04/2011", "0020", 5, 10, 15); //Venda de outro mes sem ser o mes da busca
-		
-		//ACT	
-		
-		List<Venda> vendasActual = calculadorHelper.SelecionaVendasDoMes(Vendas, mesParaSelecionar);
-		
-		//ASSERT
-		
-		assertNotNull(vendasActual);
-		assertEquals(3, vendasActual.size());		
-		
-	}
-	
-	@Test
-	public void SelecionaValoresParaAComissaoTeste(){
-		
-		String mesParaSelecionar = "03";
-		
-		List<Comissao> comissoesACalcular = calculadorHelper.SelecionaValoresParaAComissao(Vendas, Precos, Vendedores,mesParaSelecionar);
-		
-		assertNotNull(comissoesACalcular);
-		assertEquals(2, comissoesACalcular.size());
-		
-	}
+    private CalculaComissaoHelper calculadorHelper;
 
-	@Test
-	public void DeveSerPossivelSelecionarValoresParaAComissaoComTotaisCorretos(){
-		
-		String mesParaSelecionar = "03";
-				
-		double valorTotalProdutoAVendedor0020Expected= 50*40.0;
-		double valorTotalProdutoBVendedor0019Expected = 35*50;
-		
-		List<Comissao> comissoesACalcular = calculadorHelper.SelecionaValoresParaAComissao(Vendas, Precos, Vendedores,mesParaSelecionar);
-		
-		assertNotNull(comissoesACalcular);
-		
-		double totalVendedorAProdutoA = 0;
-		double totalVendedorBProdutoB= 0;
-		
-		for (Comissao comissao : comissoesACalcular){
-			
-			if(comissao.getVendedor().getCodigo().equals("0020")){
-				
-				totalVendedorAProdutoA= comissao.getTotalVendidoProdutoA();
-			}
-			else if(comissao.getVendedor().getCodigo().equals("0019")){
-				
-				totalVendedorBProdutoB = comissao.getTotalVendidoProdutoB();				
-			}			
-		}
-		
-		assertEquals(valorTotalProdutoAVendedor0020Expected, totalVendedorAProdutoA, 0.0);
-		assertEquals(valorTotalProdutoBVendedor0019Expected, totalVendedorBProdutoB, 0.0);
-	}
+    @Before
+    public void Init() throws Exception{
+        super.setUp();
+        calculadorHelper = new CalculaComissaoHelper();
+    }
 
-	
+    @Test
+    public void SelecionaVendasDoMesTeste() {
+
+        //ARRANGE		
+        String mesParaSelecionar = "03";
+
+        AdicionaUmaVendaAListaDeVendas("10/04/2011", "0020", 5, 10, 15); //Venda de outro mes sem ser o mes da busca
+
+        //ACT	
+
+        List<Venda> vendasActual = calculadorHelper.SelecionaVendasDoMes(Vendas, mesParaSelecionar);
+
+        //ASSERT
+
+        Assert.assertNotNull(vendasActual);
+        Assert.assertEquals(3, vendasActual.size());
+    }
+
+    @Test
+    public void SelecionaValoresParaAComissaoTeste(){
+
+        String mesParaSelecionar = "03";
+
+        List<Comissao> comissoesACalcular = calculadorHelper.SelecionaValoresParaAComissao(Vendas, Precos, Vendedores,mesParaSelecionar);
+
+        Assert.assertNotNull(comissoesACalcular);
+        Assert.assertEquals(2, comissoesACalcular.size());
+
+    }
+
+    @Test
+    public void DeveSerPossivelSelecionarValoresParaAComissaoComTotaisCorretos(){
+
+        String mesParaSelecionar = "03";
+
+        double valorTotalProdutoAVendedor0020Expected= 50*40.0;
+        double valorTotalProdutoBVendedor0019Expected = 35*50;
+
+        List<Comissao> comissoesACalcular = calculadorHelper.SelecionaValoresParaAComissao(Vendas, Precos, Vendedores,mesParaSelecionar);
+
+        Assert.assertNotNull(comissoesACalcular);
+
+        double totalVendedorAProdutoA = 0;
+        double totalVendedorBProdutoB= 0;
+
+        for (Comissao comissao : comissoesACalcular){
+            if(comissao.getVendedor().getCodigo().equals("0020")){
+                totalVendedorAProdutoA= comissao.getTotalVendidoProdutoA();
+            }
+            else if(comissao.getVendedor().getCodigo().equals("0019")){
+                totalVendedorBProdutoB = comissao.getTotalVendidoProdutoB();				
+            }			
+        }
+
+        Assert.assertEquals(valorTotalProdutoAVendedor0020Expected, totalVendedorAProdutoA, 0.0);
+        Assert.assertEquals(valorTotalProdutoBVendedor0019Expected, totalVendedorBProdutoB, 0.0);
+    }
 }
--- pm2012_1/test/controller/CalculaComissaoServiceTest.java
+++ pm2012_1/test/controller/CalculaComissaoServiceTest.java
@@ -8,51 +8,52 @@ import java.util.ArrayList;
 import java.util.List;
 import model.CalculaComissaoService;
 import model.entity.Comissao;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CalculaComissaoServiceTest extends CalculaComissaoTesteBase {
 	
 
-	@Test
-	public void DeveSerPossivelCalcularComissaoParaVendasDeCategoriaUm() {
-		
-		//ARRANGE			
-		
-		String mes="03";
-		
-		List<Comissao> comissoesDeCadaVendedor = new ArrayList<Comissao>();
-		
-		CalculaComissaoService calculador = new CalculaComissaoService();
-		
-		//ACT
-		
-		comissoesDeCadaVendedor = calculador.CalculaComissoes(mes,Vendas,Precos,Vendedores);
-		
-		//ASSERT
-		assertNotNull(comissoesDeCadaVendedor);
-		assertEquals(comissoesDeCadaVendedor.size(), 2);
-		assertEquals(2300.00, comissoesDeCadaVendedor.get(0).getComissao(),0.0);
-	}
+    @Test
+    public void DeveSerPossivelCalcularComissaoParaVendasDeCategoriaUm() {
 
-	@Test
-	public void DeveSerPossivelCalcularComissaoParaVendasDeCategoriaDois() {
-		
-		//ARRANGE			
-		
-		String mes="03";
-		
-		List<Comissao> comissoesDeCadaVendedor = new ArrayList<Comissao>();
-		
-		CalculaComissaoService calculador = new CalculaComissaoService();
-		
-		//ACT
-		
-		comissoesDeCadaVendedor = calculador.CalculaComissoes(mes,Vendas,Precos,Vendedores);
-		
-		//ASSERT
-		assertNotNull(comissoesDeCadaVendedor);
-		assertEquals(comissoesDeCadaVendedor.size(), 2);
-		assertEquals(1860, comissoesDeCadaVendedor.get(1).getComissao(),0.0);
-	}
+        //ARRANGE			
+
+        String mes="03";
+
+        List<Comissao> comissoesDeCadaVendedor = new ArrayList<Comissao>();
+
+        CalculaComissaoService calculador = new CalculaComissaoService();
+
+        //ACT
+
+        comissoesDeCadaVendedor = calculador.CalculaComissoes(mes,Vendas,Precos,Vendedores);
+
+        //ASSERT
+        assertNotNull(comissoesDeCadaVendedor);
+        assertEquals(comissoesDeCadaVendedor.size(), 2);
+        assertEquals(2300.00, comissoesDeCadaVendedor.get(0).getComissao(),0.0);
+    }
+
+    @Test
+    public void DeveSerPossivelCalcularComissaoParaVendasDeCategoriaDois() {
+
+        //ARRANGE			
+
+        String mes="03";
+
+        List<Comissao> comissoesDeCadaVendedor = new ArrayList<Comissao>();
+
+        CalculaComissaoService calculador = new CalculaComissaoService();
+
+        //ACT
+
+        comissoesDeCadaVendedor = calculador.CalculaComissoes(mes,Vendas,Precos,Vendedores);
+
+        //ASSERT
+        assertNotNull(comissoesDeCadaVendedor);
+        assertEquals(comissoesDeCadaVendedor.size(), 2);
+        assertEquals(1860, comissoesDeCadaVendedor.get(1).getComissao(),0.0);
+    }
 }
 
