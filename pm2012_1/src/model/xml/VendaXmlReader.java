d4be32566f2a2a5e8a746d3d8cd71230c5e24f9f
paulo.inacio@uniriotec.br <paulo.inacio@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-06-24 07:17:12 +0000

    vendasXmlReader atualizado

--- pm2012_1/src/model/xml/VendaXmlReader.java
+++ pm2012_1/src/model/xml/VendaXmlReader.java
@@ -11,40 +11,58 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import model.entity.Venda;
+import model.entity.Vendedor;
 
 /**
  *
  * @author labccet
  */
 public class VendaXmlReader {
-    public List<Venda> LeArquivoParaUmaLista(String caminhoDeOrigemXML){
+   public static List<Venda> LeArquivoParaUmaLista(String caminhoDeOrigemXML){
 		
+        List<Venda> listaXML_venda = null;
+        XMLDecoder reader = null;
+       
+        System.out.println("VendedorXmlReader");
+        
         try {
-
-            XMLDecoder vendasReaderxml = new XMLDecoder(new FileInputStream(caminhoDeOrigemXML));
-
-
-            List<Venda> listaXML = (ArrayList<Venda>) vendasReaderxml.readObject();
-
-                    for (Venda v : listaXML) {
+            try {
+                reader = new XMLDecoder(
+                        new FileInputStream(caminhoDeOrigemXML));
+                
+                Venda[] vendas;
+                listaXML_venda = new ArrayList<Venda>();
+                
+                vendas = (Venda[])reader.readObject();
+                
+                for (Venda v : vendas) {
                     System.out.println(v.getData());
                     System.out.println(v.getCodigoVendedor());
                     System.out.println(v.getQuantidadePA());
                     System.out.println(v.getQuantidadePB());
                     System.out.println(v.getQuantidadePC());
                     
-                    }
+                    Venda v2 = new Venda();
+                    v2.setData(v.getData());
+                    v2.setCodigoVendedor(v.getCodigoVendedor());
+                    v2.setQuantidadePA(v.getQuantidadePA());
+                    v2.setQuantidadePB(v.getQuantidadePB());
+                    v2.setQuantidadePC(v.getQuantidadePC());
+                            
                     
-                    List<Venda> arquivoEmListaXML = new ArrayList<Venda>();
-                    arquivoEmListaXML.addAll(listaXML);			
-                  
-                    return listaXML;
-
+                    listaXML_venda.add(v2);        
+                }
+                
+                return listaXML_venda;
+                    
+         } finally {
+               if (reader != null)
+                   reader.close();
+            }
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
             return null;
-        }
-		 
+        } 
     }
     
 }
