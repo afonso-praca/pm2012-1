224f58f7c8a7d18b89f1735b3cd6fe6f91de44d6
tais.cordeiro@uniriotec.br <tais.cordeiro@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-06-24 22:02:35 +0000

    Testes Unitários da Classe TextFileReader

---
+++ pm2012_1/test/model/txt/TextFileReaderTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.txt;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Taís
+ */
+public class TextFileReaderTest {
+    
+    
+    
+    @Test
+    /** Testa a reação do sistema  ao não informar um caminho de arquivo vazio. 
+     O resultado Esperado é criar uma lista vazia*/
+    public void testLerCaminhoVazio() { 
+        System.out.println("Objetivo do teste: Caminho de Arquivo Vazio,cria uma Lista Vazia.");
+        String caminhoDeOrigem = ""; 
+        TextFileReader instance = new TextFileReader();
+        List expResult = null;
+        List result = instance.LeArquivoParaUmaLista(caminhoDeOrigem);
+        assertEquals(expResult, result);
+    
+    }
+    
+    @Test
+    /** Testa a reação do sistema  ao fornecer um caminho de arquivo. 
+     O resultado Esperado é criar uma lista não vazia */
+    public void testLerCaminhoArquivo() throws IOException { 
+        System.out.println("Objetivo do teste:Criar uma Lista não vazia.");
+        String path = new java.io.File(".").getCanonicalPath();//Localização da raiz do projeto na máquina que está rodando o programa
+        path=path.replaceAll("\\\\", "/");  
+      
+        String caminhoDeOrigem = (path+"/src/resources/vendas.txt"); 
+        TextFileReader instance = new TextFileReader();
+        List result = instance.LeArquivoParaUmaLista(caminhoDeOrigem);
+        assertNotNull(result);
+
+    
+    }
+
+}
+
+
