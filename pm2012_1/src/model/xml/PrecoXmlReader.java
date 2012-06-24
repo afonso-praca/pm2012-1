1656beddb27345bdaf8758b2919a280ee22df9f9
paulo.inacio@uniriotec.br <paulo.inacio@uniriotec.br@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-06-24 17:50:00 +0000

    PrecoXmlReader atualizado

--- pm2012_1/src/model/xml/PrecoXmlReader.java
+++ pm2012_1/src/model/xml/PrecoXmlReader.java
@@ -9,39 +9,57 @@ import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 import model.entity.Preco;
+import model.entity.Venda;
 
 /**
  *
  * @author Marina Vinhaes
  */
 public class PrecoXmlReader {
-     public static List<Preco> LeArquivoParaUmaLista(String caminhoDeOrigemXML){
+    public static List<Preco> LeArquivoParaUmaLista(String caminhoDeOrigemXML){
 		
+        List<Preco> listaXML_preco = null;
+        XMLDecoder reader = null;
+       
+        System.out.println("VendedorXmlReader");
+        
         try {
-
-            XMLDecoder vendasReaderxml = new XMLDecoder(new FileInputStream(caminhoDeOrigemXML));
-
-
-            List<Preco> listaXML = (ArrayList<Preco>) vendasReaderxml.readObject();
-
-                    for (Preco p : listaXML) {
+            try {
+                reader = new XMLDecoder(
+                        new FileInputStream(caminhoDeOrigemXML));
+                
+                Preco[] precos;
+                
+                listaXML_preco = new ArrayList<Preco>();
+                
+                precos = (Preco[])reader.readObject();
+                
+                for (Preco p : precos) {
                     System.out.println(p.getData());
                     System.out.println(p.getPrecoPA());
                     System.out.println(p.getPrecoPB());
                     System.out.println(p.getPrecoPC());
                     
-                    }
+                    Preco p2 = new Preco();
+                    p2.setData(p.getData());
+                    p2.setPrecoPA(p.getPrecoPA());
+                    p2.setPrecoPB(p.getPrecoPB());
+                    p2.setPrecoPC(p.getPrecoPC());
                     
-                    List<Preco> arquivoEmListaXML = new ArrayList<Preco>();
-                    arquivoEmListaXML.addAll(listaXML);			
-                   
-                    return listaXML;
-
+                    
+                    listaXML_preco.add(p2);        
+                }
+                
+                return listaXML_preco;
+                    
+         } finally {
+               if (reader != null)
+                   reader.close();
+            }
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
             return null;
-        }
-		 
+        } 
     }
     
 }
