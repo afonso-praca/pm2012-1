0132396625fc4d7f7e8d04d757d41c90f24af270
afonsoinfo@gmail.com <afonsoinfo@gmail.com@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-06-24 19:13:40 +0000

    adicionado metodo toString nas entidades

--- trunk/pm2012_1/src/model/entity/Venda.java
+++ trunk/pm2012_1/src/model/entity/Venda.java
@@ -12,53 +12,57 @@ import java.util.Date;
 
 public class Venda extends BaseEntity{
 
-	private Date Data;
-	private String CodigoVendedor;
-	private int QuantidadePA;
-	private int QuantidadePB;
-	private int QuantidadePC;
+    private Date Data;
+    private String CodigoVendedor;
+    private int QuantidadePA;
+    private int QuantidadePB;
+    private int QuantidadePC;
 
-        public Venda() {
-            
-        }
+    public Venda() {
 
-        public Venda(Date Data, String CodigoVendedor, int QuantidadePA, int QuantidadePB, int QuantidadePC) {
-            this.Data = Data;
-            this.CodigoVendedor = CodigoVendedor;
-            this.QuantidadePA = QuantidadePA;
-            this.QuantidadePB = QuantidadePB;
-            this.QuantidadePC = QuantidadePC;
-        }
-        
-	public Date getData() {
-		return Data;
-	}
-	public void setData(Date data) {
-		Data = data;
-	}
-	public String getCodigoVendedor() {
-		return CodigoVendedor;
-	}
-	public void setCodigoVendedor(String codigoVendedor) {
-		CodigoVendedor = codigoVendedor;
-	}
-	public int getQuantidadePA() {
-		return QuantidadePA;
-	}
-	public void setQuantidadePA(int quantidadePA) {
-		QuantidadePA = quantidadePA;
-	}
-	public int getQuantidadePB() {
-		return QuantidadePB;
-	}
-	public void setQuantidadePB(int quantidadePB) {
-		QuantidadePB = quantidadePB;
-	}
-	public int getQuantidadePC() {
-		return QuantidadePC;
-	}
-	public void setQuantidadePC(int quantidadePC) {
-		QuantidadePC = quantidadePC;
-	}
-	
+    }
+
+    public Venda(Date Data, String CodigoVendedor, int QuantidadePA, int QuantidadePB, int QuantidadePC) {
+        this.Data = Data;
+        this.CodigoVendedor = CodigoVendedor;
+        this.QuantidadePA = QuantidadePA;
+        this.QuantidadePB = QuantidadePB;
+        this.QuantidadePC = QuantidadePC;
+    }
+
+    public Date getData() {
+            return Data;
+    }
+    public void setData(Date data) {
+            Data = data;
+    }
+    public String getCodigoVendedor() {
+            return CodigoVendedor;
+    }
+    public void setCodigoVendedor(String codigoVendedor) {
+            CodigoVendedor = codigoVendedor;
+    }
+    public int getQuantidadePA() {
+            return QuantidadePA;
+    }
+    public void setQuantidadePA(int quantidadePA) {
+            QuantidadePA = quantidadePA;
+    }
+    public int getQuantidadePB() {
+            return QuantidadePB;
+    }
+    public void setQuantidadePB(int quantidadePB) {
+            QuantidadePB = quantidadePB;
+    }
+    public int getQuantidadePC() {
+            return QuantidadePC;
+    }
+    public void setQuantidadePC(int quantidadePC) {
+            QuantidadePC = quantidadePC;
+    }
+
+    @Override
+    public String toString() {
+        return "Venda{" + "Data=" + Data + ", CodigoVendedor=" + CodigoVendedor + ", QuantidadePA=" + QuantidadePA + ", QuantidadePB=" + QuantidadePB + ", QuantidadePC=" + QuantidadePC + '}';
+    }  
 }
