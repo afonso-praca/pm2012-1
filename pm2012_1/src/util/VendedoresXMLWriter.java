b766a8502d72dba1bf7e1af61c2c1779e27ee729
afonsoinfo@gmail.com <afonsoinfo@gmail.com@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-06-24 05:52:11 +0000

    pasta util

---
+++ pm2012_1/src/util/VendedoresXMLWriter.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.beans.XMLEncoder;
+import java.io.FileOutputStream;
+import model.entity.Vendedor;
+
+/**
+ *
+ * @author Afonso
+ */
+public class VendedoresXMLWriter {
+    
+    public static void EscrevePreco(){
+        
+        Vendedor[] vendedores = new Vendedor[3];
+        vendedores[0] = new Vendedor("03", "afonso", '1');
+        vendedores[1] = new Vendedor("01", "afonso", '2');
+        vendedores[2] = new Vendedor("02", "afonso", '1');
+        
+        XMLEncoder writer = null;
+        
+        try {
+            try {
+                writer = new XMLEncoder(
+                        new FileOutputStream("objs.xml"));
+                writer.writeObject(vendedores);
+            } finally {
+                if (writer != null)
+                    writer.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+       
+}
