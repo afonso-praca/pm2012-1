6a27e44c0bb47089ff1fdb29fef78752f75c8f3b
afonsoinfo@gmail.com <afonsoinfo@gmail.com@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-06-24 19:36:35 +0000

    Aplicação funcionando com XML ou TXT

--- trunk/pm2012_1/src/controller/Controller.java
+++ trunk/pm2012_1/src/controller/Controller.java
@@ -2,6 +2,7 @@
  * Classe controladora da aplicação
  * TODO -> receber por parâmetro as referencias a leitores e escritores
  */
+
 package controller;
 
 import java.util.EventListener;
@@ -21,73 +22,57 @@ import model.xml.VendedorXmlReader;
 
 public class Controller {
         
-	public static EntityToTextFile EscritorDeArquivo;
-	public static CalculaComissaoService CalculadorDeComissaoService;
-	
-	public static List<Venda> Vendas;
-	public static List<Preco> Precos;
-	public static List<Vendedor> Vendedores;
-	public static List<Comissao> Comissoes;
-        
-        public EventListener listener;
+    public static EntityToTextFile EscritorDeArquivo;
+    public static CalculaComissaoService CalculadorDeComissaoService;
 
-	public Controller(){
-                EscritorDeArquivo = new EntityToTextFile();
-		CalculadorDeComissaoService = new CalculaComissaoService();
-	}
+    public static List<Venda> Vendas;
+    public static List<Preco> Precos;
+    public static List<Vendedor> Vendedores;
+    public static List<Comissao> Comissoes;
+
+    public EventListener listener;
+
+    public Controller(){
+            EscritorDeArquivo = new EntityToTextFile();
+            CalculadorDeComissaoService = new CalculaComissaoService();
+    }
 	
-	public void calculaComissoes(String mes, String vendasPath,
-            String precosPath, String vendedoresPath, String saidaPath, String tipo_arquivo_origem) throws Exception {
-            
-            System.out.println(tipo_arquivo_origem);
-            
+    public void calculaComissoes(String mes, String vendasPath,
+        String precosPath, String vendedoresPath, String saidaPath, String tipo_arquivo_origem) throws Exception {
+
+        System.out.println(tipo_arquivo_origem);
+
+        try {
+
+            // seleciona o tipo de leitura a ser feito (XML ou CSV)
             if (tipo_arquivo_origem.equalsIgnoreCase("txt"))
             {
-            
-		try {
-                    Vendas = VendaToEntity.getInstance().ConverteArquivoDoModelo(vendasPath);
-                    Precos = PrecoToEntity.getInstance().ConverteArquivoDoModelo(precosPath);
-                    Vendedores = VendedorToEntity.getInstance().ConverteArquivoDoModelo(vendedoresPath);
-		
-                    if (Vendas != null && Precos != null && Vendedores != null){
-                        Comissoes = CalculadorDeComissaoService.CalculaComissoes(mes, Vendas, Precos, Vendedores);
-                    } else {
-                        throw new Exception("Um erro ocoreu na leitua dos arquivos.");
-                    }
-		
-                    if (Comissoes != null){
-                        EscritorDeArquivo.ConverteComissaoParaArquivo(Comissoes, saidaPath);
-                    }
-                    else {
-                        throw new Exception("Um erro ocorreu no calculo de Comissão.");
-                    }
-		}
-		catch(Exception e){
-                    System.out.println(e.getMessage());
-                    throw e;
-		}
-            }
-            
-            if (tipo_arquivo_origem.equalsIgnoreCase("xml"))
-            {
-                System.out.println("xml!");
+                Vendas = VendaToEntity.getInstance().ConverteArquivoDoModelo(vendasPath);
+                Precos = PrecoToEntity.getInstance().ConverteArquivoDoModelo(precosPath);
+                Vendedores = VendedorToEntity.getInstance().ConverteArquivoDoModelo(vendedoresPath);
+            } else if (tipo_arquivo_origem.equalsIgnoreCase("xml")) {
                 Vendedores = VendedorXmlReader.LeArquivoParaUmaLista(vendedoresPath);
                 Vendas = VendaXmlReader.LeArquivoParaUmaLista(vendasPath);
                 Precos = PrecoXmlReader.LeArquivoParaUmaLista(precosPath);
-                
-                for (Vendedor v : Vendedores) {
-                    System.out.println(v.toString());
-                }
-                
-                for (Venda r : Vendas) {
-                    System.out.println(r.toString());
-                }
-                
-                for (Preco p : Precos) {
-                    System.out.println(p.toString());
-                }
             }
-	}
+
+            // Verifica os retornos e chama o calculo
+            if (Vendas != null && Precos != null && Vendedores != null){
+                Comissoes = CalculadorDeComissaoService.CalculaComissoes(mes, Vendas, Precos, Vendedores);
+            } else {
+                throw new Exception("Um erro ocoreu na leitua dos arquivos.");
+            }
+
+            // Escreve a resposta em arquivo TXT
+            if (Comissoes != null){
+                EscritorDeArquivo.ConverteComissaoParaArquivo(Comissoes, saidaPath);
+            } else {
+                throw new Exception("Um erro ocorreu no calculo de Comissão.");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+            throw e;
+        }
+    }
 }
-
-
