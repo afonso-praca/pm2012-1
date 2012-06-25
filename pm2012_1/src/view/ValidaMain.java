0b9d37a0ccd6be9405b201da8e118132be14a2cf
afonsoinfo@gmail.com <afonsoinfo@gmail.com@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-06-25 22:41:35 +0000

    removida funcao de validacao de mes que não é mais necessária

--- pm2012_1/src/view/ValidaMain.java
+++ pm2012_1/src/view/ValidaMain.java
@@ -1,35 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
 /**
  *
  * @author Afonso
  */
+
 public class ValidaMain {
-    
-     public static String validaMes(String mes) {
-        int mesNumero = 0;
-        String msg;
-        //usando a comparação com aspas "" para evitar null pointer exception se a variável mês vier nula
-        if (!"".equals(mes)) {
-            try {
-                mesNumero = Integer.parseInt(mes);
-            } catch (NumberFormatException e) {
-                System.out.println(e.getMessage());
-            }
-            if (mesNumero > 0 && mesNumero <=12) {
-                msg = "valido";
-            } else {
-                msg = "Mês informado é inválido!";
-            }
-        } else {
-            msg = "Preenchimento do mês é obrigatório!";
-        }
-        return msg;
-    }
      
      public static String validaArquivos(String vendas_path, String precos_path, String vendedores_path, String saida_path) {
         String msg = "";
