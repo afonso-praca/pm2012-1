47bdf280374950d3a6815dd38bcedea44c05408d
afonsoinfo@gmail.com <afonsoinfo@gmail.com@74f22b51-dc25-9aea-836a-2ddf2d299b7d>
2012-06-25 19:53:35 +0000

    ajustes de identação e espaçamentos

--- pm2012_1/src/model/CalculaComissaoCategoriaDois.java
+++ pm2012_1/src/model/CalculaComissaoCategoriaDois.java
@@ -1,31 +1,29 @@
-
 package model;
  
- import model.entity.Comissao;
+import model.entity.Comissao;
+
+/**
+ *
+ * @author Afonso
+ */
 
 public class CalculaComissaoCategoriaDois implements ICalculaComissao {
 
-	@Override
-	public void CalculaComissao(Comissao comissao) {
-		
-		double valorComissao = 0;
-		
-		double valorTotalVendas = comissao.getTotal();
-		
-		 if (valorTotalVendas <= 2000.00){
-			 valorComissao = (valorTotalVendas * 0.1); 
-          }else if((valorTotalVendas>2000.00) && 
-                  (valorTotalVendas <= 4000.00)){
-              
-              
-        	  valorComissao = (valorTotalVendas * 0.2);
-          }else if ( valorTotalVendas > 4000.00){
-        	  valorComissao = (valorTotalVendas * 0.3);
-          }
-		 
-		comissao.setComissao(valorComissao);
-		
-	}
+    @Override
+    public void CalculaComissao(Comissao comissao) {
 
+        double valorComissao = 0;
+        double valorTotalVendas = comissao.getTotal();
+
+        if (valorTotalVendas <= 2000.00){
+            valorComissao = (valorTotalVendas * 0.1); 
+        } else if ((valorTotalVendas>2000.00) && (valorTotalVendas <= 4000.00)){
+            valorComissao = (valorTotalVendas * 0.2);
+        }else if ( valorTotalVendas > 4000.00){
+            valorComissao = (valorTotalVendas * 0.3);
+        }
+
+        comissao.setComissao(valorComissao);
+    }
 }
 
--- pm2012_1/src/model/CalculaComissaoCategoriaUm.java
+++ pm2012_1/src/model/CalculaComissaoCategoriaUm.java
@@ -1,30 +1,28 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 import model.entity.Comissao;
 
+/**
+ *
+ * @author Afonso
+ */
+
 public class CalculaComissaoCategoriaUm implements ICalculaComissao {
 
-	@Override
-	public void CalculaComissao(Comissao comissao) {
-		
-		double valorComissao = 0;
-		
-		double valorTotalVendas = comissao.getTotal();
-		
-		if (valorTotalVendas > 1800.00){
+    @Override
+    public void CalculaComissao(Comissao comissao) {
+
+        double valorComissao = 0;
+        double valorTotalVendas = comissao.getTotal();
+
+        if (valorTotalVendas > 1800.00){
             valorComissao = ((valorTotalVendas - 1800) * 0.2 ) + 220;
-         }else if((valorTotalVendas>1000.00) && (valorTotalVendas <= 1800.00)){
+        } else if ((valorTotalVendas > 1000.00) && (valorTotalVendas <= 1800.00)){
             valorComissao = ((valorTotalVendas - 1000) * 0.15 ) +  100;
-         }else if ( valorTotalVendas <=1000.00){
-            valorComissao = 100;
-         }
+        } else if ( valorTotalVendas <= 1000.00){
+            valorComissao = valorTotalVendas * 0.1;
+        }
 
-		comissao.setComissao(valorComissao);
-
-	}
-
+        comissao.setComissao(valorComissao);
+    }
 }
